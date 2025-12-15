83b01f44f02c204ae69eb9b83efeaf15a33f147f
iudd <89436073@qq.com>
2025-12-16 00:12:46 +0800

    Fix Router import in main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 // Deno version of Sora2API
-import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import { load } from "https://deno.land/std@0.208.0/dotenv/mod.ts";
 import { send } from "https://deno.land/x/oak@v12.6.1/send.ts";
